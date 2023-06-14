2118c0543affc6e0714477af5ec8db44016de059
Ardee Aram <ardee.aram@gmail.com>
2023-06-14 09:49:28 +0800

    Use solid stars for the actual rating

--- exports/StarRatingCustomAttributeType.js
+++ exports/StarRatingCustomAttributeType.js
@@ -1,10 +1,15 @@
 import AttributeType from '@/components/attribute_types/AttributeType';
-import { BiStar } from "react-icons/bi";
+import { BiStar, BiSolidStar } from "react-icons/bi";
 
 const StarRatingReadOnlyComponent = ({rating}) => {
     return (
         <>
-          {Array.from({length: 5}).map(() => (
+          {Array.from({length: rating}).map(() => (
+            <span style={{fontSize: "16px"}}>
+              <BiSolidStar />
+            </span>
+          ))}
+          {Array.from({length: 5 - rating}).map(() => (
             <span style={{fontSize: "16px"}}>
               <BiStar />
             </span>
